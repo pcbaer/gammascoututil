74bfe45d060bd86e5bffb9098fe39524cab7dc25
Sascha Ternes <pcbaer@online.de>
2020-04-10 14:53:05 +0200

    View für ODL-kompatible Datenansicht erstellt.

--- tables.sql
+++ tables.sql
@@ -6,3 +6,8 @@ CREATE TABLE `log` (
   KEY `Index_interval_end` (`interval_end`),
   KEY `Index_counter` (`counter`)
 ) ENGINE=InnoDB;
+
+CREATE VIEW `view_odl` AS SELECT
+  CONCAT(DATE(`interval_start`), ' ', HOUR(`interval_start`), ':00:00') AS time,
+  (`counter` / TIMESTAMPDIFF(MINUTE, `interval_start`, `interval_end`)) / 142 AS dosage
+  FROM `log` WHERE `interval_start` >= '2020-03-25';
